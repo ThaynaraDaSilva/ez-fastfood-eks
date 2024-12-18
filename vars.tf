9a7fb3356b6d2c627d1aa584d39221f5cba3aa9f
Thaynara Silva <thaynara.silva@yaman.com.br>
2024-12-17 23:02:56 -0300

    criação de secrets variables e inclusão do provider

--- vars.tf
+++ vars.tf
@@ -72,3 +72,15 @@ variable "capacity_type"{
   description = "Tipo de capacidade"
   type        = string
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
